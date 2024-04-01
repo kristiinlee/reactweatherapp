79c1f57e6a6e5c6766181c59aefbc34fddeb2642
kristiinlee <kristiinlee@gmail.com>
2024-03-31 21:13:20 -0400

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
+      <div className="container">
+        <h1>Weather App</h1>
 
-      <footer>
-        This page was coded by <a href="https://github.com/kristiinlee" target="_blank" rel="noreferrer">Kristin Jones</a> and open-sourced on <a href="https://github.com/kristiinlee/reactweatherapp" target="_blank" rel="noreferrer">Github</a>
-      </footer>
+        <footer>
+          This page was coded by <a href="https://github.com/kristiinlee" target="_blank" rel="noreferrer">Kristin Jones</a> and open-sourced on <a href="https://github.com/kristiinlee/reactweatherapp" target="_blank" rel="noreferrer">Github</a>
+        </footer>
+      </div>
     </div>
   );
 }
